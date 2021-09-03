108fa7f6f66a98064ad44caf316fce4617530e1a
luis mauricio rojas serna <luis.3801911639@ucaldas.edu.co>
2021-09-03 00:11:37 -0500

    cambio 1

---
+++ Fibonacci/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Fibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int primerNumero = 0, segundoNumero = 1, numeroUsuario,sumaDosNumerosI, count = 0;
+           
+            Console.WriteLine("Ingrese el número de numeros de Fibonacci que desea mostrar");
+            numeroUsuario = int.Parse(Console.ReadLine());
+            
+            for (int i = 0; i < numeroUsuario; i++)
+            {
+                Console.WriteLine(primerNumero);
+                sumaDosNumerosI = primerNumero+segundoNumero;
+                primerNumero = segundoNumero;
+                segundoNumero = sumaDosNumerosI;
+
+             for (int j = 2; j < primerNumero; j++)
+                {
+
+                    if (primerNumero % i == 0)
+                    {
+                        count++;
+                    }
+                }
+            }
+
+            if (count == 1)
+            {
+                Console.WriteLine("el numero es primo");
+            }
+            else
+            {
+                Console.WriteLine("el numero no es primo");
+
+            }
+            Console.ReadKey();
+
+            
+        }
+    }
+}
